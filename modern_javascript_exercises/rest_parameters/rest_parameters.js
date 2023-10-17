06e3c976e798cb2e8a60349a5129afa71a39cdfc
Beanalini <dat826@gmail.com>
2023-10-17 16:45:23 +0100

    refactor: add() function to use rest parameters  and reduce method to add any number of passed args

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,9 +1,8 @@
 // Instructions can be found in rest_parameters.md
 
 // Add rest parameters here!
-export function add(a, b) {
-  // Add a loop here
-  return a + b;
+export function add(...numbers) {
+  return numbers.reduce((count, num) => count + num, 0);
 }
 
 add(1, 2, 3, 4, 5);
